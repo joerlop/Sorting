4d650883277b412f8bfb4cfbb865f448be071822
Geraldine Lustgarten <geraldinelustgarten@Geraldines-MacBook.local>
2019-09-09 16:13:43 -0500

    Stretch: search methods developed

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,12 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
   
+  for i in arr:
+    if arr[i] == target:
+      return i
   # TO-DO: add missing code
 
-   return -1   # not found
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -16,6 +19,14 @@ def binary_search(arr, target):
   high = len(arr)-1
 
   # TO-DO: add missing code
+  middle = (low + high) // 2
+
+  if target == arr[middle]:
+    return middle
+  elif target > arr[middle]:
+    return binary_search(arr[middle:])
+  else:
+    return binary_search(arr[0 : middle - 1])
 
   return -1 # not found
 
