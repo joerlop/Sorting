b02ef3458a32b9d96ae67aa556c53dd6e77e24cb
Geraldine Lustgarten <geraldinelustgarten@Geraldines-MacBook.local>
2019-09-09 14:33:29 -0500

    Bubble sort function developed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -21,8 +21,22 @@ def selection_sort( arr ):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
 
+    swaps = 1
+
+    while swaps > 0:
+        swaps = 0
+        for i in range(1, len(arr)):
+            if arr[i] < arr[i - 1]:
+                temp = arr[i]
+                arr[i] = arr[i - 1]
+                arr[i - 1] = temp
+                swaps += 1
+
     return arr
 
+arr = [2, 4, 10, 1, 15, 2, 30, 3, 55, 5, 89, 2000]
+
+print(bubble_sort(arr))
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
