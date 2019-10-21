20bcfeadfb3615383de0333dfb6a7117d5eabe5e
Jonathan Erlich <jj.erlich155@gmail.com>
2019-10-20 19:43:46 -0500

    Wrote iterative implementation of binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,45 +1,56 @@
-# STRETCH: implement Linear Search				
+# STRETCH: implement Linear Search
 def linear_search(arr, target):
-  
-  for i in range(len(arr)):
-    if arr[i] == target:
-      return i
-  # TO-DO: add missing code
 
-  return -1   # not found
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    # TO-DO: add missing code
 
-# STRETCH: write an iterative implementation of Binary Search 
+    return -1  # not found
+
+
+# STRETCH: write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-  if len(arr) == 0:
-    return -1 # array empty
-  
-  for i in arr:
-    middle = len(arr) // 2
+    if len(arr) == 0:
+        return -1  # array empty
 
-    if target == arr[middle]:
-      return middle
+    done = False
+    high = len(arr) - 1
+    low = 0
+    mid = (high + low) // 2
 
-  return -1 # not found
+    while done == False:
+        if len(arr) <= 1:
+            done = True
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            mid = (high + mid) // 2
+        else:
+            mid = mid // 2
+
+    return -1  # not found
 
 
-
-# STRETCH: write a recursive implementation of Binary Search 
+# STRETCH: write a recursive implementation of Binary Search
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
-  if target == arr[middle]:
-    return middle
-  elif target > arr[middle]:
-    return binary_search_recursive(arr, target, middle + 1, len(arr) - 1)
-  else:
-    return binary_search_recursive(arr, target, 0, middle)
+    middle = (low + high) // 2
 
-  return -1 # not found
+    if len(arr) == 0:
+        return -1  # array empty
+    # TO-DO: add missing if/else statements, recursive calls
+    if target == arr[middle]:
+        return middle
+    elif target > arr[middle]:
+        return binary_search_recursive(arr, target, middle + 1, len(arr) - 1)
+    else:
+        return binary_search_recursive(arr, target, 0, middle)
+
+    return -1  # not found
+
 
 arr = [1, 10, 30, 40, 50, 60, 70, 80, 90, 99]
 
